a2acbbd15a06c334d420d77313cb6739388d5376
Michlos <michlos@gmail.com>
2025-04-09 23:09:24 -0300

    feat: add GetTransactionsByUserIdController

--- src/controllers/transaction/get-transactions-by-user-id.js
+++ src/controllers/transaction/get-transactions-by-user-id.js
@@ -1,3 +1,36 @@
+import * as helpers from '../helpers/index.js';
+import * as errors from '../../errors/user.js';
+
 export class GetTransactionsByUserId {
-    async execute() {}
+    constructor(getTransactionsByUserIdUseCase) {
+        this.getTransactionsByUserIdUseCase = getTransactionsByUserIdUseCase;
+    }
+    async execute(httprequest) {
+        try {
+            const userId = httprequest.query.userId;
+            //VERIFRICAR USER ID PASADO COMO PARAMAETRO
+            if (!userId) {
+                return helpers.requiredFieldIsMissingResponse('userId');
+            }
+
+            //VERIIFCASXE SE USERID É VÁLIDO
+            const userIdIsValid = helpers.checkIfIdIsValid(userId);
+            if (!userIdIsValid) {
+                return helpers.invalidIdResponse();
+            }
+            //CHAMAWR USECASE
+            const transactions =
+                await this.getTransactionsByUserIdUseCase.execute({
+                    userId,
+                });
+            //RETORNAR RESPOSTA HTTP
+            return helpers.ok(transactions);
+        } catch (error) {
+            console.error(error);
+            if (error instanceof errors.UserNotFoundError) {
+                return helpers.userNotFoundReponse();
+            }
+            return helpers.serverError();
+        }
+    }
 }
